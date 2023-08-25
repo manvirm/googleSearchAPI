efbfbaf6c1f1b5a1e3fa450c80323e050b21308d
Manvir Mann <manvirm29@gmail.com>
2023-08-24 22:16:10 -0700

    add image search type to params

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import requests
 
 API_KEY = open('API_KEY').read()
 SEARCH_ENGINE_ID = open('SEARCH_ENGINE_ID').read()
-search_query = 'Library books'
+search_query = 'cats'
 
 url = 'hhtps://www.googleapis.com/customsearch/v1'
 params = {
     'q': search_query,
     'key': API_KEY,
-    'cx': SEARCH_ENGINE_ID
+    'cx': SEARCH_ENGINE_ID,
+    'searchType' : 'image'
 }
 
 response = requests.get(url, params=params)
-results = response.json()
-print(results)
+results = response.json()['items']
 
-if 'items' in results:
-    print(results['items'][0]['link'])
+for item in results:
+    print(item['link'])
